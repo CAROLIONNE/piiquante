e3e6ea3d659f94522fce871bbd5d807d363640b2
soc caroline <soc.caroline1991@gmail.com>
2022-01-29 00:05:49 +0100

    serveur et app

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,38 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
+const mongoose = require("mongoose");
+
+app.use(express.json());
+
+const saucesRoutes = require("./routes/sauce");
+const userRoutes = require("./routes/user");
+
+// CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+// MongoDB
+mongoose
+  .connect(
+    "mongodb+srv://caroline:lili@cluster0.pzhca.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 
+// Routes
+app.use("/api/sauces", saucesRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
