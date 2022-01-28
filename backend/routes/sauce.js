565b8156ab7ac546574d54d3a0f24870a768a337
soc caroline <soc.caroline1991@gmail.com>
2022-01-29 00:05:15 +0100

    creation routes controleurs et model sauces

---
+++ backend/routes/sauce.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const router = express.Router();
+
+const sauceCtrl = require("../controllers/sauce");
+
+router.post("/", sauceCtrl.createSauce);
+
+router.get("/", sauceCtrl.getAllSauces);
+
+router.get("/:id", sauceCtrl.getOneSauce);
+
+router.put("/:id", sauceCtrl.modifySauce);
+
+router.delete("/:id", sauceCtrl.deleteSauce);
+
+module.exports = router;
