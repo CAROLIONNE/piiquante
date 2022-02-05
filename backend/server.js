299badbbf1a2ce707ce67fa5871bac05ce444e6e
soc caroline <soc.caroline1991@gmail.com>
2022-02-05 12:10:15 +0100

    comment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
+// Import package http de node
 const http = require('http');
+
+// API
 const app = require('./app');
 
+// Port API
 app.set('port', process.env.PORT || 3000);
+
+// Creation du serveur
 const server = http.createServer(app);
 
+// Ecoute du serveur 
 server.listen(process.env.PORT || 3000);
